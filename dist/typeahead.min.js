6d0574a03939ca67ccf136b6909e4149b0914b5f
Ian White <iwhite@newrelic.com>
2014-01-09 14:04:29 -0800

    Update metadata

--- dist/typeahead.min.js
+++ dist/typeahead.min.js
@@ -1,6 +1,6 @@
 /*!
  * typeahead.js 0.9.3
- * https://github.com/newrelic/typeahead
+ * https://github.com/newrelic/typeahead.js
  * Copyright 2013 Twitter, Inc., New Relic, and other contributors; Licensed MIT
  */
 
